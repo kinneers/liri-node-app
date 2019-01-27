921a484a04ae116818dc197cf06581d55945eebf
Sarah Kinneer <kinneers@gmail.com>
2019-01-27 01:38:42 -0500

    MVP, with bonus logging

--- liri.js
+++ liri.js
@@ -9,55 +9,66 @@ var fs = require('fs');
 
 //Determines which command was used (choices are 'concert-this', 'spotify-this-song', 'movie-this', and 'do-what-it-says')
 var command = process.argv[2];
+var args;
+var text = command + ', ';
+addToLog(text);
 
 switch (command) {
     case 'concert-this':
-        concert();
+        concert(args);
         break;
     case 'spotify-this-song':
-        spotifySong();
+        spotifySong(args);
         break;
     case 'movie-this':
-        movieInfo();
+        movieInfo(args);
         break;
     case 'do-what-it-says':
-        fileCommand();
+        fileCommand(args);
         break;
     default:
         console.log("This command was not recognized.");
 }
 
-function concert() {
-    //Gets the band or artist name from args
-    var artist = process.argv.splice(3).join('+');
+function concert(args) {
+    //Checks whether or not a value for args has been previously set (by do-what-it-says), if not sets args
+    if (!args) {
+        //Gets the band or artist name from args
+        args = process.argv.splice(3).join('+');
+    }
     //Queries Bands in Town API
-    var query = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    var query = "https://rest.bandsintown.com/artists/" + args + "/events?app_id=codingbootcamp"
     //Prints list of each event's venue, location, and date to the console
     axios.get(query).then(function(res){
         for (var i = 0; i < res.data.length; i++) {
-            console.log(res.data[i].venue.name);
-            console.log(res.data[i].venue.city);
+            var venue = res.data[i].venue.name;
+            console.log(venue);
+            var city = res.data[i].venue.city
+            console.log(city);
             var date = res.data[i].datetime
             var newDate = moment(date).format('MM/DD/YYYY');
             console.log(newDate);
             console.log('------------------------------');
+            text = args + ', ' + venue + ', ' + city + ', ' + newDate + ', ';
+            addToLog(text);
         }
     });
 }
 
-function spotifySong() {
-    var song;
-    //Gets the song name from args; if no song is entered, assigns default song to "The Sign" by Ace of Base
-    if (process.argv[3]) {
-        song = process.argv.splice(3).join('+');
+function spotifySong(args) {
+    //Checks whether or not a value for args has been previously set (by do-what-it-says), if not sets args
+    if (!args) {
+        //Gets the song name from args; if no song is entered, assigns default song to "The Sign" by Ace of Base
+        if (process.argv[3]) {
+            args = process.argv.splice(3).join('+');
+        }
+        else {
+            // Narrowed choices to only 2 versions
+            args = "track:the+sign%20artist:ace+of+base%20year:2008";
+        }
     }
-    else {
-        // Narrowed choices to only 2 versions
-        song = "track:the+sign%20artist:ace+of+base%20year:2008";
-    }
-    console.log(song);
     //Displays artist(s), song name, preview link, album
-    spotify.search({ type: 'track', query: song }).then(function(response) {
+    spotify.search({ type: 'track', query: args }).then(function(response) {
         //I chose to loop through the tracks as so many songs have the same/similar titles
         for (var i = 0; i < response.tracks.items.length; i++) {
             var artists = [];
@@ -83,23 +94,27 @@ function spotifySong() {
             var albumName = response.tracks.items[i].album.name;
             console.log("Album: " + albumName);
             console.log("-----------------------------");
+            text = args + ', ' + artists + ', ' + songName + ', ' + preview + ', ' + albumName + ', ';
+            addToLog(text);
         }
     }).catch(function(err){
         console.log(err);
     });
 }
 
-function movieInfo() {
-    var movieName;
-    //Gets the movie name from args; if no movie is entered, assigns default movie to "Mr. Nobody"
-    if (process.argv[3]) {
-        movieName = process.argv.splice(3).join('+');
-    }
-    else {
-        movieName = "mr+nobody";
+function movieInfo(args) {
+    //Checks whether or not a value for args has been previously set (by do-what-it-says), if not sets args
+    if (!args) {
+        //Gets the movie name from args; if no movie is entered, assigns default movie to "Mr. Nobody"
+        if (process.argv[3]) {
+            args = process.argv.splice(3).join('+');
+        }
+        else {
+            args = "mr+nobody";
+        }
     }
     //Call to the OMDB API
-    axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(function(response) {
+    axios.get("http://www.omdbapi.com/?t=" + args + "&y=&plot=short&apikey=trilogy").then(function(response) {
         //Display Results
         console.log("------------------------------");
         var title = response.data.Title;
@@ -119,9 +134,54 @@ function movieInfo() {
         var actors = response.data.Actors;
         console.log("Actors: " + actors);
         console.log("------------------------------");
+        text = args + ', ' + title + ', ' + year + ', ' + imdbRating + ', ' + rottenTomatoes + ', ' + country + ', ' + language + ', ' + plot + ', ' + actors + ', ';
+        addToLog(text);
     });
 }
 
-function fileCommand() {
-    console.log("You will read from the file for this one");
+function fileCommand(args) {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+          return console.log(error);
+        }
+        // Split the data by commas
+        var dataArr = data.split(",");
+        // Re-display the content as an array
+        console.log(dataArr);
+        command = dataArr[0];
+        tempArgs = dataArr[1];
+        tempArgs = tempArgs.split(' ').join('+');
+        args = tempArgs.replace(/"/g,"");
+        console.log(tempArgs);
+        console.log(args);
+
+        switch (command) {
+            case 'concert-this':
+                concert(args);
+                break;
+            case 'spotify-this-song':
+                spotifySong(args);
+                break;
+            case 'movie-this':
+                movieInfo(args);
+                break;
+            default:
+                console.log("This command was not recognized.");
+        }
+      });
+}
+
+//Function to append content to log.txt
+function addToLog(text) {
+    fs.appendFile("log.txt", text, function(err) {
+        // If an error was experienced we will log it.
+        if (err) {
+          console.log(err);
+        }
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        else {
+          console.log("Content Added!");
+        }
+      });
 }
