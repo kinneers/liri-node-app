d16df3ac7c9cc955b35d382418c30bfe38583cf1
Sarah Kinneer <kinneers@gmail.com>
2019-01-26 22:04:27 -0500

    Spotify working, have artist- working on remaining data displays

--- liri.js
+++ liri.js
@@ -1,7 +1,9 @@
+// Read and set environment variables
 require('dotenv').config();
 
 var keys = require('./keys.js');
 
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 var axios = require('axios');
@@ -24,4 +26,24 @@ if (command === 'concert-this') {
             console.log('------------------------------');
         }
     })
+}
+
+if (command === 'spotify-this-song') {
+    console.log('Working so far')
+    var song;
+
+    //Assigns song the value of arguments after 2; if no song is entered, assigns default song to "The Sign"
+    if (process.argv[3]) {
+        song = process.argv.splice(3).join('+');
+    }
+    else {
+        song = "the+sign%20ace+of+base%20year:2008";
+    }
+    console.log(song);
+    spotify.search({ type: 'track', query: song }).then(function(response) {
+        var artists = response.tracks.items[0].artists[0].name;
+        console.log(artists);
+    }).catch(function(err){
+        console.log(err);
+    });
 }
